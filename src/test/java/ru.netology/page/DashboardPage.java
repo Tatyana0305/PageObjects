4c4f1d007e621fbbacd5501fe5de693f411b174a
Tatyana Bondarenko <tanya_kursavka@mail.ru>
2022-04-19 08:22:24 +0300

    added right test

---
+++ src/test/java/ru.netology/page/DashboardPage.java
@@ -0,0 +1,31 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DashboardPage {
+    private SelenideElement heading = $("[data-test-id=dashboard]");
+    private ElementsCollection cards = $$(".list__item div");
+    private final String balanceStart = "баланс: ";
+    private final String balanceFinish = " р.";
+
+    public DashboardPage() {
+        heading.shouldBe(visible);
+    }
+
+    public int getCardBalance(int index) {
+        var text = cards.get(index).text();
+        return extractBalance(text);
+    }
+
+    private int extractBalance(String text) {
+        var start = text.indexOf(balanceStart);
+        var finish = text.indexOf(balanceFinish);
+        var value = text.substring(start + balanceStart.length(), finish);
+        return Integer.parseInt(value);
+    }
+}
---
+++ src/test/java/ru.netology/test/MoneyTransferTest.java
@@ -0,0 +1,30 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.page.DashboardPage;
+import ru.netology.page.LoginPage;
+import ru.netology.page.MoneyTransferPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MoneyTransferTest {
+    @Test
+    void shouldTransferMoneyBetweenOwnCards() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPage();
+//    var loginPage = open("http://localhost:9999", LoginPageV2.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+        new MoneyTransferPage().transferMoney("100");
+        var actual = new DashboardPage().getCardBalance(0);
+        int expected = 10100;
+        assertEquals(expected, actual);
+        new MoneyTransferPage().defaultMoney("100");
+    }
+
+
+}
