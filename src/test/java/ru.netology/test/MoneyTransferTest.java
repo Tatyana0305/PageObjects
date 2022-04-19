ec32a268967756091dde863589286ee74a966031
Tatyana Bondarenko <tanya_kursavka@mail.ru>
2022-04-19 08:34:10 +0300

    added failing test

--- src/test/java/ru.netology/test/MoneyTransferTest.java
+++ src/test/java/ru.netology/test/MoneyTransferTest.java
@@ -1,11 +1,15 @@
 package ru.netology.test;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataHelper;
 import ru.netology.page.DashboardPage;
 import ru.netology.page.LoginPage;
 import ru.netology.page.MoneyTransferPage;
 
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -13,6 +17,7 @@ public class MoneyTransferTest {
     @Test
     void shouldTransferMoneyBetweenOwnCards() {
         open("http://localhost:9999");
+        Configuration.holdBrowserOpen = true;
         var loginPage = new LoginPage();
 //    var loginPage = open("http://localhost:9999", LoginPageV2.class);
         var authInfo = DataHelper.getAuthInfo();
@@ -26,5 +31,22 @@ public class MoneyTransferTest {
         new MoneyTransferPage().defaultMoney("100");
     }
 
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsOverBalance() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPage();
+//    var loginPage = open("http://localhost:9999", LoginPageV2.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+        new MoneyTransferPage().transferMoney("20000");
+        var actual = new DashboardPage().getCardBalance(0);
+        int expected = 10000;
+        assertEquals(expected, actual);
+        new MoneyTransferPage().defaultMoney("20000");
+        $(withText("Недостаточно средств для перевода!")).shouldBe(Condition.visible);
+
+    }
 
 }
